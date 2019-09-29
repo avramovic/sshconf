173793ddb4434f2818a348507470256c7229ff45
Nemanja Avramović <avramyu@gmail.com>
2019-09-29 13:39:01 +0200

    feat(core): Added "view" command to show single connection with all data

---
+++ Commands/ViewHostCommand.php
@@ -0,0 +1,50 @@
+<?php namespace Commands;
+
+use Symfony\Component\Console\Helper\Table;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputDefinition;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class ViewHostCommand extends BaseCommand
+{
+    protected static $defaultName = 'view';
+
+    protected function configure()
+    {
+        $this->setDescription('Lists existing SSH connections')
+            ->setDefinition(
+                new InputDefinition([
+                    new InputArgument('host', InputArgument::REQUIRED),
+                ])
+            );
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $this->appName($output);
+
+        $host = $input->getArgument('host');
+
+        if (!$this->sshConf->has($host)) {
+            $this->error(sprintf('SSH entry "%s" does not exist, use: sshconf ls', $host));
+        }
+
+        $entry = $this->sshConf->get($host);
+
+        foreach ($entry as $key => $value) {
+            $data[] = [strtoupper($key), $value];
+        }
+
+        $table = new Table($output);
+
+        $table
+            ->setHeaders(['Option', 'Value'])
+            ->setRows($data);
+
+        $table
+            ->setHeaderTitle('SSH: '.$host)
+            ->setFooterTitle('Total: '.count($data))
+            ->render();
+    }
+}
--- sshconf.php
+++ sshconf.php
@@ -8,12 +8,14 @@ use Commands\AddHostCommand;
 use Commands\EditHostCommand;
 use Commands\ListHostsCommand;
 use Commands\RemoveHostCommand;
+use Commands\ViewHostCommand;
 use Symfony\Component\Console\Application;
 
-$application = new Application("sshconf", "0.1");
+$application = new Application("sshconf", "0.2");
 
 $application->add(new AddHostCommand());
 $application->add(new EditHostCommand());
+$application->add(new ViewHostCommand());
 $application->add(new ListHostsCommand());
 $application->add(new RemoveHostCommand());
 
